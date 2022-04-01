38405f513625092a9a9a78d627d177c0b1cc8aa3
Denis Kud <dekud2109@gmail.com>
2022-04-01 16:57:51 +0300

    Added registration fields Values added to "Registration Controller"

--- src/main/java/com/example/coursework/controller/RegistrationController.java
+++ src/main/java/com/example/coursework/controller/RegistrationController.java
@@ -1,17 +1,17 @@
 package com.example.coursework.controller;
 
-import javafx.application.Application;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class RegistrationController {
-    public static Stage stage;
+
+    private static Stage stage;
 
     public static Stage getStage() {
         return stage;
@@ -28,19 +28,78 @@ public class RegistrationController {
     private URL location;
 
     @FXML
-    private TextField emailField;
+    private TextField emailBackupField;
 
     @FXML
-    private PasswordField passwordField;
+    private TextField phoneNumberBeckupField;
+
+    @FXML
+    private TextField FirstNameTranslitField;
+
+    @FXML
+    private TextField LastNameTranslitField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField midlNameField;
+
+    @FXML
+    private ComboBox<String> groupComoBox;
+
+    @FXML
+    private RadioButton radioButtonStudentRole;
+
+    @FXML
+    private RadioButton radioButtonTeacherRole;
+
+    @FXML
+    private ComboBox<?> orgUnitPathComboBox;
+
+    @FXML
+    private DatePicker dateOfEntry;
+
+    @FXML
+    private DatePicker releaseDate;
 
     @FXML
     private Button registrationButton;
+
+    @FXML
+    private TextArea commentField;
+
+    @FXML
+    private TextField dateRegistrationField;
 
     @FXML
     void initialize() {
         registrationButton.setOnAction(event -> {
             stage.close();
         });
+
+        dateRegistrationField.textProperty()
+                .set(LocalDateTime.now().format(
+                        DateTimeFormatter.ofPattern
+                                ("yyyy-MM-dd | HH:mm")));
+
+        dateOfEntry.setOnAction(event -> {
+            System.out.println(dateOfEntry.getValue());
+        });
+
+        releaseDate.setOnAction(event -> {
+            System.out.println(releaseDate.getValue());
+        });
+        groupComoBox.getItems().addAll("ПС4-1", "ПС3-1", "ПС2-1", "ПС1-1");
+        groupComoBox.setOnAction(event -> {
+            System.out.println(groupComoBox.getValue());
+        });
+        radioButtonStudentRole.setToggleGroup(toggleGroup);
+        radioButtonTeacherRole.setToggleGroup(toggleGroup);
     }
 
+    private static final ToggleGroup toggleGroup = new ToggleGroup();
 }
